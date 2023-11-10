53901a14d6a96582bf665b3ce711b8f08284c637
Victoria <dolefuariaq@mail.ru>
2023-11-10 23:17:01 +0700

    add fly, draw skt and dirt,

---
+++ enemy.py
@@ -0,0 +1,39 @@
+from strategy import MovementStrategy
+import strategy
+from os import path
+import pygame
+
+class Enemy:
+    x: float
+    y: float
+    rect: pygame.Rect
+    image: pygame.Surface
+    movement_strategy: MovementStrategy
+
+    def __init__(self, x,y,image,movement_stategy):
+         self.x = x
+         self.y = y
+         self.rect = image.get_rect()
+         self.image = image
+         self.movement_strategy = movement_stategy
+
+    def update(self):
+        self.movement_strategy.move(self)
+        self.rect.bottomleft = (int(self.x),int(self.y))
+    
+
+def new_bush(x,y,speed_x):
+    _image = pygame.image.load(path.join('assets', 'rosebush.png'))
+    image = pygame.transform.scale_by(_image, 2)
+    movement_strategy = strategy.BushMovementStrategy(speed_x)
+    return Enemy(x, y, image, movement_strategy)
+
+def new_fly(x,y,speed_x):
+    _image = pygame.image.load(path.join('assets', 'fly.png'))
+    image = pygame.transform.scale_by(_image, 2)
+    max_y = 25
+    period = 1
+    movement_strategy = strategy.FlyMovementStrategy(speed_x, max_y, period)
+    return Enemy(x, y, image, movement_strategy)
+
+   
---
+++ strategy.py
@@ -0,0 +1,37 @@
+import math
+class MovementStrategy:
+   def move(self, enemy):
+       pass
+
+
+class BushMovementStrategy(MovementStrategy):
+   speed: float
+
+   def __init__(self, speed):
+         self.speed = speed
+
+
+   def move(self, enemy):
+         enemy.x -= self.speed
+    
+
+class FlyMovementStrategy(MovementStrategy):
+   speed_x: float
+   max_y: float
+   frequency: float
+   FPS: float
+   frame: float
+
+   def __init__(self, speed_x,max_y, period):
+         self.speed_x = speed_x
+         self.frequency = 2*math.pi / period
+         self.FPS = 60.0
+         self.max_y = max_y/self.FPS
+         self.frame = 0.0
+
+   def move(self, enemy):
+         enemy.x -= self.speed_x
+         time = self.frame / self.FPS
+         speed_y = self.max_y* self.frequency * math.cos(time*self.frequency)
+         enemy.y += speed_y
+         self.frame += 1
